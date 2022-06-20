36d65adc813296b07505243b621bcd3f62f12df7
Schifferking <schifferking@hotmail.com>
2022-06-20 14:19:32 -0500

    Generate code
    Created a generate_code method in the Mastermind class to pick 6 random
    unique values from the COLORS array. Also added more colors to that
    constant and a getter method to access it.

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,11 @@
 class Mastermind
+  attr_reader :COLORS
+
   def initialize
-    @COLORS = ['red', 'green', 'blue', 'pink', 'yellow', 'purple', 'orange', 'black', 'white']    
+    @COLORS = ['red', 'green', 'blue', 'pink', 'yellow', 'purple', 'orange', 'black', 'white', 'brown', 'gray']
+  end
+
+  def generate_code
+    @COLORS.sample(6)
   end
 end
