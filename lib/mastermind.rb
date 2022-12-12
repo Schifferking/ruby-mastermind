b06bd66ed7e9f2160d4db553c8090bc32fea7492
Schifferking <schifferking@hotmail.com>
2022-12-12 15:18:07 -0600

    Move Player class to player.rb

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,30 +1,5 @@
 require_relative '../lib/generable'
-
-class Player
-  include Generable
-
-  attr_accessor :guess_code, :role, :code
-
-  def initialize
-    @role = ''
-  end
-
-  def obtain_code
-    @code = generate_code
-  end
-
-  def enter_code
-    @guess_code = []
-
-    while guess_code.length < CODE_LENGTH
-      print 'Enter a color: '
-      color = gets.chomp.downcase
-      guess_code.append(color) if COLORS.include?(color) && !guess_code.include?(color)
-    end
-
-    @guess_code
-  end
-end
+require_relative '../lib/player'
 
 class Human < Player
   def initialize
---
+++ lib/player.rb
@@ -0,0 +1,25 @@
+class Player
+  include Generable
+
+  attr_accessor :guess_code, :role, :code
+
+  def initialize
+    @role = ''
+  end
+
+  def obtain_code
+    @code = generate_code
+  end
+
+  def enter_code
+    @guess_code = []
+
+    while guess_code.length < CODE_LENGTH
+      print 'Enter a color: '
+      color = gets.chomp.downcase
+      guess_code.append(color) if COLORS.include?(color) && !guess_code.include?(color)
+    end
+
+    @guess_code
+  end
+end
