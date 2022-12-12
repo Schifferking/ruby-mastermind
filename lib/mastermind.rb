45105c2c66f40bd986a2b1b43c32787e32583ae8
Schifferking <schifferking@hotmail.com>
2022-12-12 17:07:50 -0600

    Add print_role_prompt_message method

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -103,6 +103,10 @@ class Mastermind
   def print_welcome_message
     puts 'Welcome to a new mastermind game!'
   end
+
+  def print_role_prompt_message
+    puts "Please select your role. Type 'creator' or 'guesser'"
+  end
 end
 
 h = Human.new
