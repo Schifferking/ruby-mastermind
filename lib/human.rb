0f0aaca80c88c4fd3d73dd3dc9284b3653ace835
Schifferking <schifferking@hotmail.com>
2022-12-13 15:19:32 -0600

    Delete old code

--- lib/human.rb
+++ lib/human.rb
@@ -1,14 +1,6 @@
 require_relative '../lib/player'
 
 class Human < Player
-  def initialize
-  end
-
-  def obtain_code
-    puts "Please enter a #{CODE_LENGTH} colors code"
-    @code = enter_code
-  end
-
   def enter_input
     gets.chomp.downcase
   end
