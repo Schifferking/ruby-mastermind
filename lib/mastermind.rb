3e4762ff28a8d89570d6951a02503c4cbcfbd6af
Schifferking <schifferking@hotmail.com>
2023-01-03 12:50:12 -0600

    Move print_welcome_message method to printable.rb
    Add print_welcome_message method in printable.rb and delete it in
    mastermind.rb.

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -12,10 +12,6 @@ class Mastermind
     set_game
   end
 
-  def print_welcome_message
-    puts 'Welcome to a new mastermind game!'
-  end
-
   def print_role_prompt_message
     puts "Please select your role. Type 'creator' or 'guesser'"
   end
--- lib/printable.rb
+++ lib/printable.rb
@@ -1,2 +1,5 @@
 module Printable
+  def print_welcome_message
+    puts 'Welcome to a new mastermind game!'
+  end
 end
