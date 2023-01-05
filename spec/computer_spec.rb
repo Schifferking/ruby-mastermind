f23a57090d0e837d41c4729c045afdbe7a376aee
Schifferking <schifferking@hotmail.com>
2023-01-05 15:20:21 -0600

    Add test for shuffle_code? method

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,10 +3,20 @@ require_relative '../lib/computer'
 describe Computer do
   describe '#create_code' do
     subject(:computer_creating_code) { described_class.new }
-    
+
     it 'calls the generate_code method' do
       expect(computer_creating_code).to receive(:generate_code)
       computer_creating_code.create_code
+    end
+  end
+
+  describe '#shuffle_code?' do
+    context 'when there is at least one nil value in code' do
+      subject(:computer_one_nil) { described_class.new }
+
+      it 'returns false' do
+        expect(computer_one_nil.shuffle_code?).to be false
+      end
     end
   end
 end
